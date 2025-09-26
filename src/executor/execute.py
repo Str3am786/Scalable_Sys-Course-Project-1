72df4391a649d4346638f3829eb8af853b8532c6
Str3am786 <arroyo.m786@gmail.com>
2025-09-26 15:22:27 +0300

    change to b end in 7,8,9, now accepts any id as long as it ends in 7,8,9. Before it was checking for ids that were just 7 8 or 9

--- src/executor/execute.py
+++ src/executor/execute.py
@@ -6,6 +6,7 @@ from opencep.condition.CompositeCondition import AndCondition
 from opencep.condition.BaseRelationCondition import EqCondition
 from opencep.condition.Condition import Variable, SimpleCondition
 from opencep.condition.KCCondition import KCIndexCondition
+from opencep.misc.ConsumptionPolicy import *
 from src.executor.runner import Runner
 
 # PATTERN SEQ (BikeTrip+ a[], BikeTrip b)
@@ -16,7 +17,7 @@ structure = SeqOperator(
     KleeneClosureOperator(
         PrimitiveEventStructure(event_type="BikeTrip", name="a"),
         min_size=1,
-        max_size=8,   # tune
+        max_size=5,          # <<< important
     ),
     PrimitiveEventStructure(event_type="BikeTrip", name="b"),
 )
@@ -34,27 +35,47 @@ chain_contiguity = KCIndexCondition(
     relation_op=lambda curr, prev: curr[0] == prev[1],
     offset=-1
 )
+
+# Fix: KC 'a' is a list -> fetch last element's bike_id
 last_a_bike_vs_b_bike = EqCondition(
     Variable("b", lambda ev: ev["bike_id"]),
-    Variable("a", lambda lst: lst[-1]["bike_id"])    # KC list -> last
+    Variable("a", lambda lst: lst[-1]["bike_id"]),
 )
 
-
-b_in_targets = SimpleCondition(
-    Variable("b", lambda ev: ev["end_station_id"]),
-    relation_op=lambda s: s in {7, 8, 9}
+# b_in_targets = SimpleCondition(
+#     Variable("b", lambda ev: ev["end_station_id"]),
+#     relation_op=lambda s: s in {7, 8, 9}
+# )
+b_last_digit_in_7_8_9 = SimpleCondition(
+    Variable("b", lambda ev: (ev["end_station_id"] % 10) if ev.get("end_station_id") is not None else None),
+    relation_op=lambda d: d in {7, 8, 9}
 )
 
+consumption = ConsumptionPolicy(
+    primary_selection_strategy=SelectionStrategies.MATCH_ANY,
+    secondary_selection_strategy=SelectionStrategies.MATCH_NEXT,
+    single=["BikeTrip"]   # apply to all BikeTrip events (your whole pattern)
+)
 
 hot_paths_pattern = Pattern(
     structure,
-    AndCondition(same_bike_chain, chain_contiguity, last_a_bike_vs_b_bike, b_in_targets),
-    timedelta(hours=1)
+    AndCondition(same_bike_chain, chain_contiguity, last_a_bike_vs_b_bike, b_last_digit_in_7_8_9),
+    timedelta(hours=1),
+    consumption_policy=consumption
 )
 
-def execute(pattern=hot_paths_pattern, bursty=False, limit=100):
+
+
+# hot_paths_pattern = Pattern(
+#     structure,
+#     AndCondition(same_bike_chain, chain_contiguity, last_a_bike_vs_b_bike, b_in_targets),
+#     timedelta(hours=1)
+# )
+
+
+def execute(pattern=hot_paths_pattern, bursty=False, limit=None):
     try:
-        runner = Runner(pattern=pattern, bursty=bursty, limit=100)
+        runner = Runner(pattern=pattern, bursty=bursty, limit=limit)
         runner.run()
     except:
         pass
