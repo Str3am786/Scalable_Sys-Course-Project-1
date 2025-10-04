5827d3c1cdc45c9b7aa79f702e75d39f65debd54
ChecchinLu <luca.checchin@aalto.fi>
2025-10-04 20:00:08 +0300

    Fixed reader

--- src/scalable_system/ingestion/input_reader.py
+++ src/scalable_system/ingestion/input_reader.py
@@ -2,9 +2,12 @@ import csv, time
 from dateutil import parser as dtparse
 from datetime import timezone
 from typing import Optional
-from ..io.redis_client import get_client
+from ..io.redis_client import get_client, get_default_client
 from ..config import N_SHARDS, STREAM_PREFIX
 from ..core.sharding import shard_for_bike, stream_name
+from pathlib import Path
+import os
+from typing import List
 
 def now_ms(): return int(time.time()*1000)
 
@@ -17,30 +20,63 @@ def to_int_safe(v):
     try: return int(v)
     except: return None
 
-def produce_csv(path: str, mapping: dict, n_shards: Optional[int] = None, max_rows: Optional[int] = None):
-    r = get_client()
+def get_input_files(dir : str) -> List[str]:
+    month_dirs = [ file for file in os.listdir(path=dir)]
+    files = []
+
+    for d in month_dirs:
+        i = int(d.split("_")[0])
+        filename = os.listdir(Path(dir,d))[0]
+        relative_path = Path(dir,d,filename)
+        files.append(relative_path)
+        
+    return files
+    
+    
+def produce_csv(dir_path : str, n_shards: Optional[int] = None, max_rows: Optional[int] = None):
+    
+    mapping = {
+        "bike_id": "Bike ID",
+        "start_station_id": "Start Station ID",
+        "end_station_id": "End Station ID",
+        "started_at": "Start Time",
+        "ended_at" :  "Stop Time"
+        
+    }
+
+    i_files = get_input_files(dir_path)
+    r = get_default_client()
+    print(r.ping())
     n = 0
     nsh = n_shards or N_SHARDS
-    with open(path, newline="") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            bike_id = row.get(mapping["bike_id"])
-            if not bike_id: continue
-            start_sid = to_int_safe(row.get(mapping["start_station_id"]))
-            end_sid   = to_int_safe(row.get(mapping["end_station_id"]))
-            if start_sid is None or end_sid is None: continue
-            started_ms = parse_ts_ms(row.get(mapping["started_at"]))
-            ended_ms   = parse_ts_ms(row.get(mapping["ended_at"]))
-            shard = shard_for_bike(bike_id, nsh)
-            sname = stream_name(STREAM_PREFIX, shard)
-            r.xadd(sname, {
-                b"bike_id": bike_id.encode(),
-                b"start_station_id": str(start_sid).encode(),
-                b"end_station_id": str(end_sid).encode(),
-                b"started_at_ms": str(started_ms).encode(),
-                b"ended_at_ms": str(ended_ms).encode(),
-                b"ingest_ts_ms": str(now_ms()).encode(),
-            })
-            n += 1
-            if max_rows and n >= max_rows: break
+    for file in i_files:
+        with open(file, newline="") as f:
+            reader = csv.DictReader(f)
+            
+            for row in reader:
+                bike_id = row.get(mapping["bike_id"])
+                if not bike_id: continue
+                start_sid = to_int_safe(row.get(mapping["start_station_id"]))
+                end_sid   = to_int_safe(row.get(mapping["end_station_id"]))
+                if start_sid is None or end_sid is None: continue
+                started_ms = parse_ts_ms(row.get(mapping["started_at"]))
+                ended_ms   = parse_ts_ms(row.get(mapping["ended_at"]))
+                shard = shard_for_bike(bike_id, nsh)
+                sname = stream_name(STREAM_PREFIX, shard)
+                
+                res = r.xadd(sname, {
+                    b"bike_id": bike_id.encode(),
+                    b"start_station_id": str(start_sid).encode(),
+                    b"end_station_id": str(end_sid).encode(),
+                    b"started_at_ms": str(started_ms).encode(),
+                    b"ended_at_ms": str(ended_ms).encode(),
+                    b"ingest_ts_ms": str(now_ms()).encode(),
+                })
+                                
+                n += 1
+                if max_rows and n >= max_rows: break
+            
     print(f"Produced {n} events into {nsh} shards.")
+
+
+
--- src/scalable_system/io/redis_client.py
+++ src/scalable_system/io/redis_client.py
@@ -4,3 +4,7 @@ from ..config import REDIS_URL
 def get_client() -> redis.Redis:
     # decode_responses=False to keep bytes and avoid accidental type coercion
     return redis.from_url(REDIS_URL, decode_responses=False)
+
+def get_default_client() -> redis.Redis:
+    return redis.Redis(host='localhost', port=6379, decode_responses=True)
+
